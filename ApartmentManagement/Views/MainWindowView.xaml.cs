6f3d558939639f675e35cfa4684a2ee7d15d6d8f
Loc <trantanloc20045@gmail.com>
2025-03-16 15:17:11 +0700

    First commit

---
+++ ApartmentManagement/Views/MainWindowView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ApartmentManagement.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindowView.xaml
+    /// </summary>
+    public partial class MainWindowView : Window
+    {
+        public MainWindowView()
+        {
+            InitializeComponent();
+        }
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtSearch.Visibility = Visibility.Collapsed;
+        }
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            txtSearch.Visibility = Visibility.Visible;
+        }
+    }
+}
